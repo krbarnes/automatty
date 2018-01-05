c5ed8f74d1578570347760371bda079c4b8230fb
Kevin Barnes <kevin.barnes@shopify.com>
2018-01-05 17:10:28 -0500

    point at the right sheet

--- auto_matty.rb
+++ auto_matty.rb
@@ -25,14 +25,17 @@ post '/fulfilled' do
 	nickname = nicknames[prng.rand(nicknames.length) - 1]
 
 	envConfig = ENV['DRIVE']
-	io = envConfig ? StringIO.new(envConfig) : "AutoMatty-fe567505bfbf.json"
+
+	io = envConfig = nil ? StringIO.new(envConfig) : "AutoMatty-fe567505bfbf.json"
 
 	session = GoogleDrive::Session.from_service_account_key(io)
-	sheet = session.spreadsheet_by_key("1I-jychFZpkSVI8oZoMv_aBepDc-ZOXEovtinFeAa0Dg").worksheets[0]
+
+	sheet = session.spreadsheet_by_url('https://docs.google.com/spreadsheets/d/1q1haSv60cbKH_vPuZ9Iz82glrsF3jGZKVFwIc2kHLPw/edit').worksheets[0]
 
 	line_items = data['line_items']
 	line_items.each do |item|
-		row = sheet.num_rows + 1
+		row = 2
+		sheet.insert_rows(row, 1)
 		sheet[row, 1] = item["title"]
 		sheet[row, 2] = total_price
 		sheet[row, 3] = "#{first_name} \"#{nickname}\" #{last_name}"
